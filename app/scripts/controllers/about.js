731c504b978d2ce94cd3428541dbad016650e507
Evie Caldwell-Horne <evcaldwell@gmail.com>
2015-10-20 18:58:30 -0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name paintbrushApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the paintbrushApp
+ */
+angular.module('paintbrushApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name paintbrushApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the paintbrushApp
+ */
+angular.module('paintbrushApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
